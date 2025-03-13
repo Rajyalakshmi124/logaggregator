2785e08c8e87799226885dc23908fed972e03471
Rajyalakshmi Vemula <rajyalakshmi.vemula@theelixrlabs.com>
2025-03-13 14:23:24 +0530

    Maintained folder structure and I maintain the constants in Alphabetic order

--- main.py
+++ main.py
@@ -1,37 +1,19 @@
-#os module to interact with system files,checks if a folder exists or list files
-import os
-from constants import *
-
-#it prompts the user to enter the folder path
-folder_path=input("Enter the folder path: ")
-#checks if the given folder path is a vaild directory or not
-if not os.path.isdir(folder_path):
-    print(f"{INVALID_FOLDER_PATH}") 
-else:
-    # if file exists this retrieves a list of files in the folder
-    files=os.listdir(folder_path)
-
-    #checks if the folder is empty 
-    if not files: 
-        print(f"{EMPTY_FOLDER}")
+#importing the file_validation function from the file_validation module
+from file_validation.file_validation import file_validation
+#main entry point of the script
+if __name__ == "__main__":
+    #prompt the user to enter the folder path   
+    folder_path = input("Enter the folder path: ")
+    
+    #It returns an error message the list of log files, the count of log files, and the count of invalid files
+    error_message, log_files, log_file_count, invalid_file_count = file_validation(folder_path)
+    # If there's an error message, print it
+    if error_message:
+        print(error_message)
+    # Otherwise, print the count of log files and invalid files
     else:
-        print(f"{PROCESSING}")
-        #count .log files, ensure a proper count of .log files in the given folder
-        log_file_count=0
-        #initialize counter for non-log files, count non-log files
-        invalid_file_found=0
-        #loop through each file in the folder
-        for file in files:
-            if file.endswith('.log'):#checks if the file has a .log extension 
-                
-                log_file_count = log_file_count+1 # increment log file count
-            else:
-                invalid_file_found = invalid_file_found+1 #increment invalid file count
-        if log_file_count>0:#check if there are any log files
-            print(f"TOTAL_LOG_FILE_FOUND: {log_file_count:}")
-            print(f"INVALID_FOLDER_COUNT: {invalid_file_found:}")
-        else:
-            print(f"TOTAL_LOG_FILE_FOUND: {log_file_count}")
-            print(f"INVALID_FOLDER_COUNT: {invalid_file_found}")
-            print(f"{NO_LOG_FILES}")
+        print(f"Total Log files found: {log_file_count}")
+        print(f"Invalid files found: {invalid_file_count}")
+ 
+        
         
